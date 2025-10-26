0dc4f915e27221ea76fa4ceedaacb2a91946cdfb
n3rada <72791564+n3rada@users.noreply.github.com>
2025-10-26 20:54:43 +0100

    Add Info action for retrieving SQL Server instance information using DMVs and SERVERPROPERTY

--- mssqlclient_ng/src/actions/database/__init__.py
+++ mssqlclient_ng/src/actions/database/__init__.py
@@ -6,9 +6,11 @@ Database actions for SQL Server database management.
 from mssqlclient_ng.src.actions.database.dabases import Databases
 from mssqlclient_ng.src.actions.database.xprocs import ExtendedProcs
 from mssqlclient_ng.src.actions.database.impersonate import Impersonation
+from mssqlclient_ng.src.actions.database.info import Info
 
 __all__ = [
     "Databases",
     "ExtendedProcs",
     "Impersonation",
+    "Info",
 ]
