87a3ba65a69065cf5480d73dae23988d856cfc9b
n3rada <72791564+n3rada@users.noreply.github.com>
2025-10-26 20:51:54 +0100

    Add Impersonation action for checking SQL Server impersonation permissions

--- mssqlclient_ng/src/actions/database/__init__.py
+++ mssqlclient_ng/src/actions/database/__init__.py
@@ -5,8 +5,10 @@ Database actions for SQL Server database management.
 # Import actions to trigger registration with ActionFactory
 from mssqlclient_ng.src.actions.database.dabases import Databases
 from mssqlclient_ng.src.actions.database.xprocs import ExtendedProcs
+from mssqlclient_ng.src.actions.database.impersonate import Impersonation
 
 __all__ = [
     "Databases",
     "ExtendedProcs",
+    "Impersonation",
 ]
