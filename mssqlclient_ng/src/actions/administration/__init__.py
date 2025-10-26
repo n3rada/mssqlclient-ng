1ef993d6000ce6fa96865441eaa31f0c128843e2
n3rada <72791564+n3rada@users.noreply.github.com>
2025-10-26 20:03:22 +0100

    Add CreateUser action for creating SQL Server logins with server role privileges

--- mssqlclient_ng/src/actions/administration/__init__.py
+++ mssqlclient_ng/src/actions/administration/__init__.py
@@ -5,8 +5,10 @@ Administration actions for SQL Server management.
 # Import actions to trigger registration with ActionFactory
 from mssqlclient_ng.src.actions.administration.configure import Configure
 from mssqlclient_ng.src.actions.administration.sessions import Sessions
+from mssqlclient_ng.src.actions.administration.createuser import CreateUser
 
 __all__ = [
     "Configure",
     "Sessions",
+    "CreateUser",
 ]
