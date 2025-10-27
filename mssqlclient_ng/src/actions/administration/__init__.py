4f8ef32b9a31a6c73d1fb77ee999903d434784e6
n3rada <72791564+n3rada@users.noreply.github.com>
2025-10-28 00:07:35 +0100

    Add Kill action to terminate specific or all SQL Server sessions

--- mssqlclient_ng/src/actions/administration/__init__.py
+++ mssqlclient_ng/src/actions/administration/__init__.py
@@ -7,10 +7,12 @@ from mssqlclient_ng.src.actions.administration.configure import Configure
 from mssqlclient_ng.src.actions.administration.sessions import Sessions
 from mssqlclient_ng.src.actions.administration.createuser import CreateUser
 from mssqlclient_ng.src.actions.administration.monitor import Monitor
+from mssqlclient_ng.src.actions.administration.kill import Kill
 
 __all__ = [
     "Configure",
     "Sessions",
     "CreateUser",
     "Monitor",
+    "Kill",
 ]
