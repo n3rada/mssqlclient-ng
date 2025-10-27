b539b7bd3d0419c8ae82cf605e986922985cf8b0
n3rada <72791564+n3rada@users.noreply.github.com>
2025-10-27 20:51:54 +0100

    Add RemoteProcedureCall action for managing RPC Out on linked servers

--- mssqlclient_ng/src/actions/network/__init__.py
+++ mssqlclient_ng/src/actions/network/__init__.py
@@ -2,8 +2,10 @@
 
 from mssqlclient_ng.src.actions.network.smb_coerce import SmbCoerce
 from mssqlclient_ng.src.actions.network.links import Links
+from mssqlclient_ng.src.actions.network.rpc import RemoteProcedureCall
 
 __all__ = [
     "SmbCoerce",
     "Links",
+    "RemoteProcedureCall",
 ]
