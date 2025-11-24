5a06973aecb2e90cf25918724bae576a8d8f5289
n3rada <72791564+n3rada@users.noreply.github.com>
2025-11-24 15:43:05 +0000

    feat: remove unused Remove action from filesystem actions

--- mssqlclient_ng/src/actions/filesystem/__init__.py
+++ mssqlclient_ng/src/actions/filesystem/__init__.py
@@ -1,6 +1,5 @@
 from mssqlclient_ng.src.actions.filesystem.file_read import FileRead
-from mssqlclient_ng.src.actions.filesystem.rm import Remove
 from mssqlclient_ng.src.actions.filesystem.tree import Tree
 from mssqlclient_ng.src.actions.filesystem.upload import Upload
 
-__all__ = ["FileRead", "Remove", "Tree", "Upload"]
+__all__ = ["FileRead", "Tree", "Upload"]
